90f0c404aa389cddb7a8de4fc20998aa83dd564e
xiaobog <garsonguo@163.com>
2018-10-18 15:43:47 +0800

    pug模板

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 2. 初始化package.json
     + npm init
     + package.json中scripts上添加启动脚本
+3. 模板pug
+    + 安装模板pug npm i pug -S
+    + 安装koa-views npm i koa-views -S
+    + 使用参照index.js
--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const Koa = require('Koa')
 const app = new Koa()
+const views = require('koa-views')
+const {
+    resolve
+} = require('path')
 
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
 app.use(async (ctx, next) => {
-    ctx.body = 'hi'
+    await ctx.render('index', {
+        you: 'nhao'
+    })
 })
 
 app.listen(3000)
